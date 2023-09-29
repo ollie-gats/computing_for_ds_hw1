d3744d05967ddd3b5f713ce1ed1a6fd94ed2c9b2
adgianv <a.digianvito@hotmail.it>
2023-09-29 15:03:12 +0200

    questions 1-3 angelo

---
+++ hw1_first_questions.py
@@ -0,0 +1,16 @@
+# 1)
+def triple(x):
+    return(3*x)
+
+
+# 2)
+def subtract(a, b):
+    return (a-b)
+
+
+# 3)
+def dictionary_maker(my_list):
+    my_dictionary = {}
+    for key, value in my_list:
+        my_dictionary[key] = value
+    return(my_dictionary)
